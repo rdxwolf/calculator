f9444a27587552fb9cc02d8b6c6087dead78f627
Gurinder and Sharma <akarsh4211@gmail.com>
2026-06-23 22:16:03 +0530

    fixed some minor bugs and add favicon

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-// calculator prgram
-
 var display = document.getElementById("display");
 function appendToDisplay(input) {
-    if (display.value === "Error") {
-        display.value = "";
-    }
-    display.value += input;
-}
-function clearDisplay() {
+  if (display.value === "Error") {
     display.value = "";
+  }
+
+  const operators = ["+", "-", "*", "/"];
+  const lastChar = display.value.slice(-1);
+  if (operators.includes(lastChar) && operators.includes(input)) {
+    display.value = display.value.slice(0, -1) + input;
+  } else {
+    display.value += input;
+  }
+}
+
+function clearDisplay() {
+  display.value = "";
 }
 function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = "Error";
-    }
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
 }
